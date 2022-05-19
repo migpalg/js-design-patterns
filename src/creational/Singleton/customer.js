c76caad2ccdc3827c2ffaa2896d9882359c2d336
Miguel González <m.palacio@globant.com>
2022-05-19 15:35:55 -0500

    feat: mentoring session

--- src/creational/Singleton/customer.js
+++ src/creational/Singleton/customer.js
@@ -1,9 +1,9 @@
 const FoodLogger = require("./foodLogger");
 
-foodLogger = new FoodLogger().getFoodLoggerSingleton();
+var outerFoodLogger = new FoodLogger().getFoodLoggerSingleton();
 
 class Customer {
-  constructor(order) {
+  constructor({ order, foodLogger }) {
     this.price = order.price;
     this.food = order.foodItem;
     foodLogger.log(order);
