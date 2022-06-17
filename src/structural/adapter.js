bfe565ef1f69ef868f1285d24702f4eafde63941
Luis Piedrahita <luis.piedrahita@autodesk.com>
2022-06-17 10:48:57 -0300

    feature(adapter):add adapter implementation

---
+++ src/structural/adapter.js
@@ -0,0 +1,26 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+function area(rectangle) {
+  return rectangle.width * rectangle.height;
+}
+
+class SquareToRectangleAdapter {
+  constructor(square) {
+    this.square = square;
+  }
+
+  get width() {
+    return this.square.side;
+  }
+
+  get height() {
+    return this.square.side;
+  }
+}
+
+let sq = new Square(11);
+area(new SquareToRectangleAdapter(sq));
