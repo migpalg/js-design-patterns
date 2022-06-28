2f51345984effbafaddb7fe7e0385e100ef02dd4
Luis Piedrahita <luis.piedrahita@autodesk.com>
2022-06-28 19:16:02 -0300

    feat(facade):add classes and tests

---
+++ src/structural/facade/facade.test.js
@@ -0,0 +1,16 @@
+import { MagicSquareGenerator } from './facade';
+
+describe('facade', function () {
+  it('should generate good magic squares', function () {
+    let gen = new MagicSquareGenerator();
+    let square = gen.generate(3);
+
+    let adder = function (p, c) {
+      return p + c;
+    };
+
+    let first = square[0].reduce(adder);
+
+    for (let row of square) expect(row.reduce(adder)).toEqual(first);
+  });
+});
